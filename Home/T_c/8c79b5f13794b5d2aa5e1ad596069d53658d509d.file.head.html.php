d3947aa89cbf297438596de73acc2d307556032b
Jinsong Bai <bjstry@163.com>
2015-05-25 09:55:54 -0400

    15525-955-uppage

---
+++ Home/T_c/8c79b5f13794b5d2aa5e1ad596069d53658d509d.file.head.html.php
@@ -0,0 +1,61 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-05-25 21:53:38
+         compiled from "Home/T/default/theme/head.html" */ ?>
+<?php /*%%SmartyHeaderCode:10467438325563296278d6b8-38195824%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '8c79b5f13794b5d2aa5e1ad596069d53658d509d' => 
+    array (
+      0 => 'Home/T/default/theme/head.html',
+      1 => 1432558474,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '10467438325563296278d6b8-38195824',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_55632962870197_70216118',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_55632962870197_70216118')) {function content_55632962870197_70216118($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf8>
+<link rel='stylesheet' href='<?php echo @constant('ROOT');?>
+/Public/uikit/css/uikit.almost-flat.min.css'>
+<link rel='stylesheet' href='<?php echo @constant('ROOT');?>
+/Public/uikit/css/components/notify.min.css'>
+<link rel='stylesheet' href='<?php echo @constant('ROOT');?>
+/Public/uikit/css/components/search.min.css'>
+<link rel='sytlesheel' href='<?php echo @constant('ROOT');?>
+/Public/css/main.css'>
+<?php echo '<script'; ?>
+ type='text/javascript' src='<?php echo @constant('ROOT');?>
+/Public/js/jquery.min.js'><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type='text/javascript' src='<?php echo @constant('ROOT');?>
+/Public/uikit/js/uikit.min.js'><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type='text/javascript' src='<?php echo @constant('ROOT');?>
+/Public/uikit/js/components/notify.min.js'><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type='text/javascript' src='<?php echo @constant('ROOT');?>
+/Public/uikit/js/components/search.min.js'><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type='text/javascript' src='<?php echo @constant('ROOT');?>
+/Public/js/form.js'><?php echo '</script'; ?>
+>
+<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<?php }} ?>
